25df9bc5194cb0ce971ff5fa60d47b93a9de691f
el-zach <el-zach@web.de>
2019-10-05 17:47:34 +0200

    Added Mouseborderpan instead of input.getaxis

--- LD45/Assets/Scripts/CameraControls.cs
+++ LD45/Assets/Scripts/CameraControls.cs
@@ -18,12 +18,42 @@ public class CameraControls : MonoBehaviour
     void Update()
     {
         Vector3 mov = Vector3.zero;
-        mov += Camera.main.transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        mov += Camera.main.transform.right * GetMouseBorderX() * speed * Time.deltaTime;
         Vector3 forward = Camera.main.transform.forward;
         forward.y = 0f;
         forward = forward.normalized;
-        mov += forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
-
+        mov += forward * GetMouseBorderY() * speed * Time.deltaTime;
         controlledTarget.Translate(mov);
     }
+
+    float GetMouseBorderX()
+    {
+        Vector3 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        Debug.Log(mousePos);
+        if (mousePos.x <= 0.1f)
+        {
+            return -1f;
+        }
+        else if (mousePos.x >= 0.9f)
+        {
+            return 1f;
+        }
+        else return 0f;
+    }
+
+    float GetMouseBorderY()
+    {
+        Vector3 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        if (mousePos.y <= 0.1f)
+        {
+            return -1f;
+        }
+        else if (mousePos.y >= 0.9f)
+        {
+            return 1f;
+        }
+        else return 0f;
+    }
+
+
 }
