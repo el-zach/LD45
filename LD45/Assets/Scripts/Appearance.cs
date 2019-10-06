31f81b22dfb6d1beb570aaeab1b223fe5d883e83
el-zach <el-zach@web.de>
2019-10-06 11:36:36 +0200

    added face variants

---
+++ LD45/Assets/Scripts/Appearance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Appearance : MonoBehaviour
+{
+    [Header("FaceSpawner")]
+    public Transform graphicsContainer;
+    public Transform headBone;
+    public GameObject[] faceVariants;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnFace();
+    }
+
+    void SpawnFace()
+    {
+        int rand = Random.Range(0, faceVariants.Length);
+        var go = Instantiate(faceVariants[rand], graphicsContainer);
+        go.GetComponent<FollowTransform>().target = headBone;
+    }
+}
